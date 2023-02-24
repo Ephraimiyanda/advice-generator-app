b3d9b88c683f40e69fc05a5e051c326cb1c322c4
Ephraim Iyanda <113598104+Ephraimiyanda@users.noreply.github.com>
2023-02-24 19:59:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const adviceId = document.querySelector(".adviceId");
+const adviceRef = document.querySelector(".adviceRef");
+const dice = document.querySelector(".dice");
+
+function getAdvice(){
+  const advicePromise = fetch('https://api.adviceslip.com/advice');
+
+advicePromise
+.then((response)=> response.json())
+.then((data)=>{
+  console.log(data) 
+ 
+const {slip:{id, advice}}= data;
+adviceId.textContent=id;
+adviceRef.textContent=`"${advice}"`
+})
+
+
+}
+dice.addEventListener("click", getAdvice)
